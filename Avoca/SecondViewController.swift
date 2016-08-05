a9c69c4b74460fad8f33654be212d73983f8a64e
Lakshay Khatter <lakshaykhatter@gmail.com>
2016-08-05 19:21:59 -0400

    Initial Commit

---
+++ Avoca/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Avoca
+//
+//  Created by Lakshay Khatter on 8/5/16.
+//  Copyright © 2016 Lakshay Khatter. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
